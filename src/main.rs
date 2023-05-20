c11f655e64c3b02e0c650a87e7464208338adb86
fzdwx <likelovec@gmail.com>
2023-05-20 22:19:38 +0800

    feat: call `execve` run command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::{ffi::CString, os::raw::c_char};
+
 use clap::Parser;
+use libc::execve;
 
 fn main() {
     Cli::parse().run();
@@ -14,8 +17,29 @@ struct Cli {
 }
 
 impl Cli {
-    fn run(&self) {
+    pub fn run(&self) {
         println!("Command: {}", self.command);
         println!("Args: {:?}", self.args);
+        self.to_c_command();
+    }
+
+    fn to_c_command(&self) {
+        let mut args = vec![
+            CString::new(self.command.as_str()).unwrap(),
+        ];
+
+        if let Some(self_args) = &self.args {
+            for arg in self_args {
+                args.push(CString::new(arg.as_str()).unwrap());
+            }
+        }
+
+        let argv: Vec<*const c_char> = args.iter().map(|arg| arg.as_ptr()).collect();
+        let envp: *const *const c_char = std::ptr::null();
+
+        unsafe {
+            let rs = libc::execve(argv[0], argv.as_ptr(), envp);
+            println!("rs: {}", rs);
+        }
     }
 }
